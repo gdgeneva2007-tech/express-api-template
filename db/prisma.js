2bef5d4f3206d356ac39d73eb42dae3c2bb06545
gdgeneva2007-tech <251467665+gdgeneva2007-tech@users.noreply.github.com>
2026-05-19 16:09:30 +0800

    Fix bugs

--- db/prisma.js
+++ db/prisma.js
@@ -1,6 +1,11 @@
 // db/prisma.js
-const { PrismaClient } = require('@prisma/client')
-
-const prisma = new PrismaClient()
-
-module.exports = prisma
+require("dotenv").config();
+const { PrismaClient } = require("@prisma/client");
+const { PrismaPg } = require("@prisma/adapter-pg");
+const connectionString = process.env.DATABASE_URL;
+if (!connectionString) {
+  throw new Error("DATABASE_URL must be set to use Prisma");
+}
+const adapter = new PrismaPg(connectionString);
+const prisma = new PrismaClient({ adapter });
+module.exports = prisma;
